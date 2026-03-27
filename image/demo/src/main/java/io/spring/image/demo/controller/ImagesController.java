8292429c5123f7dcbddda600a6d6a7d6ad90c30e
CHZINNn <henrique.carlos@estudante.ifmt.edu.br>
2026-03-27 12:28:32 -0400

    Add files via upload

--- image/demo/src/main/java/io/spring/image/demo/controller/ImagesController.java
+++ image/demo/src/main/java/io/spring/image/demo/controller/ImagesController.java
@@ -1,7 +1,6 @@
 package io.spring.image.demo.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +10,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/upload")
+@Slf4j
 public class ImagesController {
 
     // {"name": "", "size":100} //application/json
